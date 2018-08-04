1fda9a57312380d8716cc807aa08520d73829017
Koti Vellanki <vellankikoti@gmail.com>
2018-08-04 20:23:45 +0530

    Update guessing_number.py

--- guessing_number.py
+++ guessing_number.py
@@ -1,21 +1,22 @@
 import random
-print('What is Your Name?')
-name = input()
-print('Hello,' + name + ' I\'m thinking a number between 1 and 100')
+
+name = input("What is Your Name?")
+print('Hello,' + name + ' I\'m thinking a number between 1 and 100\n Could you Guess it?')
+
 guesses = []
 
 secret_number = random.randint(1,100)
+entered_number = int(input("Guess a number between 1-100: "))
+guesses.append(entered_number)
 
-player = int(input("Guess a number between 1-100: "))
-guesses.append(player)
-
-while player != secret_number:
-    if player > secret_number:
+while entered_number != secret_number:
+    if entered_number > secret_number:
         print("Number is too High!")
     else:
         print("Number is too Low!")
-    player = int(input("Guess a number between 0-100: "))
-    guesses.append(player)
+        
+    entered_number = int(input("Guess a number between 0-100: "))
+    guesses.append(entered_number)
 
 else:
     print("Congrats, You guessed it!")
