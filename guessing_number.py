938aed04f18d2b743a9d274226683b3e38cef49b
Koti Vellanki <vellankikoti@gmail.com>
2018-08-04 19:49:17 +0530

    Update guessing_number.py

--- guessing_number.py
+++ guessing_number.py
@@ -1,19 +1,24 @@
 import random
 print('What is Your Name?')
 name = input()
-secret_number = random.randint(1,50)
-print('Hell0,' + name + ' I\'m thinking a number between 1 and 50')
-#Ask the Player to guess 5 times
-for guesses in range(1,6):
-	print('Take a Guess!')
-	guess = int(input())
-	if guess < secret_number:
-		print('Your guess is lower than my number')
-	elif guess > secret_number:
-		print('Your guess is higher than my number')
-	else:
-		break
-if guess == secret_number:
-	print('Good Job!'+ name +'You guessed my number in'+ str(guesses) +' guesses' )
+secret_number = random.randint(1,100)
+print('Hello,' + name + ' I\'m thinking a number between 1 and 100')
+guesses = []
+
+secret_number = random.randint(1,100)
+
+player = int(input("Guess a number between 1-100: "))
+guesses.append(player)
+
+while player != secret_number:
+    if player > secret_number:
+        print("Number is too High!")
+    else:
+        print("Number is too Low!")
+    player = int(input("Guess a number between 0-100: "))
+    guesses.append(player)
+
 else:
-	print('Nope,You failed to guess,my guessed number was' + str(secret_number))
+    print("Congrats, You guessed it!")
+    print("It took you {} Guesses to find".format(len(guesses)))
+    print("The guesses you tried are: ", guesses)
